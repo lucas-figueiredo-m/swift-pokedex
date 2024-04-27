010886bd0ebf03ac4bf2f231d2779e90c3f514dd
Lucas Figueiredo <lfigueiredo@upstart13.com>
2024-04-27 18:50:54 -0300

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Lucas Martins Figueiredo on 27/04/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
